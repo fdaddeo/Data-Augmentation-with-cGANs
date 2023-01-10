0dea887266f0b4d0b1022c39a26d4cf48d1fb376
Pippo <filo.dadde99@gmail.com>
2023-01-10 20:12:37 +0100

    Normalization fix

--- train_classifier.py
+++ train_classifier.py
@@ -48,7 +48,7 @@ def main(args):
     transformList = transforms.Compose([# transforms.Resize(config['image_size']),
                                         transforms.Resize(224),
                                         transforms.ToTensor(),
-                                        transforms.Normalize(mean=(0.4915, 0.4823, 0.4468), std=(0.2470, 0.2435, 0.2616))])
+                                        transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
 
     cifar10_trainset = CIFAR10(root=config['dataset_path'],
                               train=True,
