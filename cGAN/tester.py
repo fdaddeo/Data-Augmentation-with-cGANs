d42864e3722c0fa91f57e3aca8fdfbb25275f4e7
Pippo <filo.dadde99@gmail.com>
2023-01-14 19:47:44 +0100

    Bug fixes

--- cGAN/tester.py
+++ cGAN/tester.py
@@ -46,10 +46,10 @@ class Tester(object):
         self.generator.load_state_dict(torch.load(self.config['gen_model_path']))
 
         # Define a fixed noise to be used as starting point for generation
-        self.fixed_noise = torch.randn(1, self.config['gen']['latentspace_dim'], 1, 1, device=device)
+        self.fixed_noise = torch.randn(self.config['class_images'], self.config['gen']['latentspace_dim'], 1, 1, device=device)
 
         # Define a vector to encode the labels
-        self.labels = torch.zeros(1, self.config['num_label'])
+        self.labels = self.label_preprocessing()
 
         self.folders_preprocess()
 
@@ -61,6 +61,21 @@ class Tester(object):
         for idx, class_name in enumerate(self.config['classes']):
             os.makedirs(os.path.join(self.config['generated_images_path'], class_name), exist_ok=True)
 
+    def label_preprocessing(self):
+        """
+        Label preprocessing for the generator. Basically, are needed onehot vectors in order to encode the labels.
+
+        Returns
+        -------
+        A vector containing the onehot encoding of each label.
+        """
+
+        labels = torch.zeros(self.config['num_label'], self.config['num_label'])
+        labels = labels.scatter_(1, torch.LongTensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).view(self.config['num_label'], 1), 1)
+        labels = labels.view(self.config['num_label'], self.config['num_label'], 1, 1)
+
+        return labels
+
     def generate(self):
         """
         Function that generates test samples starting from fixed noise.
@@ -70,15 +85,18 @@ class Tester(object):
         The generated images.
         """
         
+        self.generator.eval()
+
         with torch.no_grad():
             # Generate a number 'class_images' of images for each class
             for idx, class_name in enumerate(self.config['classes']):
-                # Fill 'labels' accordingly to the wanted class
-                label_gen = self.labels.index_fill_(1, torch.tensor([0, idx]), 1).view(self.config['num_label'], 1)
-                label_gen = label_gen.view(1, self.config['num_label'], 1, 1).to(self.device)
+                label = (torch.ones(self.config['class_images']) * idx).type(torch.LongTensor) # [0,0,0,0,...] -> torch.Size([class_images])
+                label_gen = self.labels[label].to(self.device) # -> torch.Size([class_images, 10, 1, 1])
+                output = self.generator(self.fixed_noise, label_gen) # -> torch.Size([class_images, 3, image_w, image_h])
 
-                for i_image in range(self.config['class_images']):
-                    output = self.generator(self.fixed_noise, label_gen)
-                    result_path = os.path.join(os.path.join(self.config['generated_images_path'], class_name), f"image_{i_image + 1}.jpg")
-                    vutils.save_image(output.data.cpu(), result_path, nrow=1, padding=0)
+                for i, image in enumerate(output):
+                    result_path = os.path.join(os.path.join(self.config['generated_images_path'], class_name), f"image_{i + 1}.jpg")
+                    vutils.save_image(image.data.cpu(), result_path, nrow=1, padding=0, normalize=True)
                     print(f"Saved generated image into {result_path}...")
+
+        self.generator.train()
